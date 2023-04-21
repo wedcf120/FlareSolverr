833a502c776a46332d738e8e45aab1e3a485c2b8
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-21 20:04:38 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -8,7 +8,7 @@ import subprocess
 subprocess.Popen(['sudo', 'python', 'src/flaresolverr.py'])
 
 html = os.system ("sleep 20 && curl  'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{  \"cmd\": \"request.get\",  \"url\":\"https://sharemania.us/\",  \"maxTimeout\": 60000}'")
-
+print(html)
 data = json.loads(html)           # 解析 JSON 数据
 response = data.get('response')   # 获取 response 数据
 print(response)                   # 输出 response 数据
