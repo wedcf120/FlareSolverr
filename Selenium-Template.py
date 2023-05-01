f7af40a91c144c78edd4c40e86a40addeded5e63
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-05-01 10:45:23 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -12,7 +12,7 @@ from datetime import datetime
 time.sleep(32)
 
 # 使用 subprocess 模块调用 curl 命令，并捕获命令输出结果
-curl_cmd = "curl 'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{\"cmd\": \"request.get\",\"url\":\"https://google.com/\",\"maxTimeout\": 60000}'"
+curl_cmd = "curl 'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{\"cmd\": \"request.get\",\"url\":\"https://sharemania.us/\",\"maxTimeout\": 60000}'"
 result = subprocess.check_output(curl_cmd, shell=True)
 
 # 解析 JSON 数据
