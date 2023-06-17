e06fc3dc0b79a5aec8e6aede1fcb4d90f238d3fb
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-06-17 22:04:22 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -15,7 +15,7 @@ time.sleep(36)
 # 使用 subprocess 模块调用 curl 命令，并捕获命令输出结果
 curl_cmd = "curl 'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{\"cmd\": \"request.get\",\"url\":\"" + sys.argv[1] + "\",\"maxTimeout\": 60000, \"proxy\": { \"url\": \"http://127.0.0.1:1085\" }}'"
 result = subprocess.check_output(curl_cmd, shell=True)
-os.system("curl -x 127.0.0.1:1085 -L https://billboard.com/")
+
 
 # 解析 JSON 数据
 data = json.loads(result.decode('utf-8'))
