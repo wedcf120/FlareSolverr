b378c138b65a025568afbf389da61f2e4c889a81
mxsfgnoixer <127394502+mxsfgnoixer@users.noreply.github.com>
2023-08-04 13:02:19 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -12,6 +12,9 @@ from datetime import datetime
 # 睡眠 20 秒以确保 flaresolverr.py 已经启动
 time.sleep(36)
 
+#判断代理有没有用
+os.system("curl --proxy 127.0.0.1:1085 https://proapi.115.com/app/uploadinfo")
+
 # 使用 subprocess 模块调用 curl 命令，并捕获命令输出结果
 curl_cmd = "curl 'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{\"cmd\": \"request.get\",\"url\":\"" + sys.argv[1] + "\",\"maxTimeout\": 60000, \"proxy\": { \"url\": \"http://127.0.0.1:1085\" }}'"
 result = subprocess.check_output(curl_cmd, shell=True)
