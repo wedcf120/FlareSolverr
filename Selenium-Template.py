735c40c843a0d4a47a62e8eecf876fde91f2e063
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-09-05 12:14:37 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -26,5 +26,16 @@ response = data.get("solution", {}).get("response")
 print(response)  # 输出 response 数据
 
 
+# 使用正则表达式从结果中提取图片地址
+pattern = r'https://img\.supjav\.com/images/.+?\.(jpg|png|bmp)'
+matches = re.finditer(pattern, result)
+for match in matches:
+    img_url = match.group()
+    # 构建第二个 curl 命令来下载图片
+    img_filename = img_url.split('/')[-1]
+    curl_cmd_2 = f"curl 'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{{\"cmd\": \"request.get\",\"url\":\"{img_url}\",\"maxTimeout\": 60000, \"proxy\": {{ \"url\": \"http://127.0.0.1:1085\" }} }}' > {img_filename}"
+    subprocess.run(curl_cmd_2, shell=True)
+    os.system("pkill chrome; pkill chromedriver")
+
 
 os.system("pkill chrome; pkill chromedriver")
